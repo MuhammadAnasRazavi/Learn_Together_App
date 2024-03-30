c9aafeea8de9e0ebf0eafb0b94bee8fa81042d50
MuhammadAnasRazavi <muhammadanasrazavi@gmail.com>
2024-03-30 11:56:06 +0530

    Changed text composables font size and text alignment.

--- app/src/main/java/com/example/learntogetherapp/MainActivity.kt
+++ app/src/main/java/com/example/learntogetherapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,7 +16,10 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogetherapp.ui.theme.LearnTogetherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    ArticlePage()
                 }
             }
         }
@@ -44,13 +48,19 @@ fun Article(image: Painter, title: String, shortDesc: String, longDesc: String, 
             contentScale = ContentScale.FillWidth
         )
         Text(
-            text = title
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
         )
         Text(
-            text = shortDesc
+            text = shortDesc,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
         )
         Text(
-            text = longDesc
+            text = longDesc,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
         )
     }
 }
@@ -61,11 +71,12 @@ fun ArticlePage(modifier: Modifier = Modifier) {
         image = painterResource(id = R.drawable.bg_compose_background),
         title = stringResource(R.string.title_text),
         shortDesc = stringResource(R.string.shortDesc_text),
-        longDesc = stringResource(R.string.longDesc_text)
+        longDesc = stringResource(R.string.longDesc_text),
+        modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun ArticlePagePreview() {
     LearnTogetherAppTheme {
